5aa73c8b04f7baf565d527ad409d0fac7cf4172d
nyrta1 <nurtai_2005@mail.ru>
2023-12-06 15:38:47 +0600

    refactor: converter values have been added and optimized

---
+++ src/main/java/com/example/youdown/extractors/FileSizeConverter.java
@@ -0,0 +1,10 @@
+package com.example.youdown.extractors;
+
+public class FileSizeConverter {
+    public static String convertToRoundedMB(Double value) {
+        if (value == null) {
+            return "N/A";
+        }
+        return String.format("%.2f MB", Math.round(value * 100.0) / 100.0);
+    }
+}
---
+++ src/main/java/com/example/youdown/extractors/TimeConverter.java
@@ -0,0 +1,14 @@
+package com.example.youdown.extractors;
+
+public class TimeConverter {
+    public static String convertSecondsToFormattedTime(int seconds) {
+        if (seconds < 0) {
+            return "00:00";
+        }
+
+        int minutes = seconds / 60;
+        int remainingSeconds = seconds % 60;
+
+        return String.format("%02d:%02d", minutes, remainingSeconds);
+    }
+}
