8090b2a8387424e6334431473c616856ce8ad445
nyrta1 <nurtai_2005@mail.ru>
2023-12-14 00:15:02 +0600

    test: added tests for containers confirming that it is available to run

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM openjdk:17-jdk-alpine
-WORKDIR /app
 COPY ./target/youdown-0.0.1-SNAPSHOT.jar app.jar
 EXPOSE 8080
+
+RUN apk update && apk upgrade && apk add --no-cache ffmpeg
+
 CMD ["java", "-jar", "app.jar"]
--- src/test/java/com/example/youdown/YoudownApplicationTests.java
+++ src/test/java/com/example/youdown/YoudownApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.youdown;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@Testcontainers
 class YoudownApplicationTests {
+	@Container
+	private static final GenericContainer<?> spring_boot_container = new GenericContainer<>("youdown-spring-boot-backend:latest")
+			.withExposedPorts(8080);
+
+	@Container
+	private static final GenericContainer<?> angular_app_container = new GenericContainer<>("youdown-angular-frontend:latest")
+			.withExposedPorts(4200);
+
+	@BeforeAll
+	public static void beforeAll() {
+		spring_boot_container.start();
+		angular_app_container.start();
+	}
+
+	@AfterAll
+	public static void afterAll() {
+		spring_boot_container.stop();
+		angular_app_container.stop();
+	}
 
 	@Test
-	void contextLoads() {
-
+	public void testContainerIsRunning() {
+		assertTrue(spring_boot_container.isRunning());
+		assertTrue(angular_app_container.isRunning());
 	}
 }
