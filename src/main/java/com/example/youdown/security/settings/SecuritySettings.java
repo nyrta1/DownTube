bc1a570f7d04606d234742fcde1cc7b700ee6263
Nurtai <147030027+nyrta1@users.noreply.github.com>
2023-12-15 05:55:08 +0600

    Update SecuritySettings.java

--- src/main/java/com/example/youdown/security/settings/SecuritySettings.java
+++ src/main/java/com/example/youdown/security/settings/SecuritySettings.java
@@ -1,2 +1,20 @@
-package com.example.youdown.security.settings;public class SecuritySettings {
+package com.example.youdown.security.settings;
+
+public class SecuritySettings {
+    public final static String[] YOUTUBE_MEDIA_FILE_DOWNLOADER_API = {
+            "/media/download/video-with-audio",
+            "/media/download/audio",
+            "/media/download/video",
+            "/media/download/merged-audio-with-video"
+    };
+
+    public final static String[] YOUTUBE_PARSED_JSON_DATA_SENDER = {
+            "/json/downloader/all",
+            "/json/downloader/playlist"
+    };
+
+    public final static String[] ALLOW_ERROR_PAGES_LIST = {
+            "/error/**"
+    };
 }
+
