8e1ebf4837dd7fd5de4c9e72edc56aa556175aad
Nurtai <147030027+nyrta1@users.noreply.github.com>
2023-12-15 05:54:50 +0600

    Update SpringSecurityConfig.java

--- src/main/java/com/example/youdown/security/config/SpringSecurityConfig.java
+++ src/main/java/com/example/youdown/security/config/SpringSecurityConfig.java
@@ -1,2 +1,36 @@
-package com.example.youdown.security.config;public class SpringSecurityConfig {
+package com.example.youdown.security.config;
+
+import com.example.youdown.security.settings.SecuritySettings;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeRequests(auth -> auth
+                        .requestMatchers(SecuritySettings.YOUTUBE_PARSED_JSON_DATA_SENDER).permitAll()
+                        .requestMatchers(SecuritySettings.YOUTUBE_MEDIA_FILE_DOWNLOADER_API).permitAll()
+                        .requestMatchers(HttpMethod.GET, SecuritySettings.ALLOW_ERROR_PAGES_LIST).permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
+
