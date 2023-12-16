26c69d818445c3b5583726ebcee54aa769178ca1
nyrta1 <nurtai_2005@mail.ru>
2023-12-16 20:14:06 +0600

    fix: [angular-js] fixed generation of different encrypted values from crypto service

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # YouDown
 Youtube video and audio downloader.
 This project is purely for educational purposes.
-
-####  256-bit WEP Secret Key:462CBD3D2172B4FC858DAB3D3C998
--- src/main/angular16/src/app/services/crypto/crypto.service.ts
+++ src/main/angular16/src/app/services/crypto/crypto.service.ts
@@ -5,12 +5,10 @@ import * as CryptoJS from 'crypto-js';
   providedIn: 'root'
 })
 export class CryptoService {
-  private readonly SECRET_KEY_256_BIT_WEP: string = "462CBD3D2172B4FC858DAB3D3C998";
-
   constructor() { }
 
   encrypt(value: string): string {
-    const encrypted = CryptoJS.AES.encrypt(value, this.SECRET_KEY_256_BIT_WEP).toString();
+    const encrypted = CryptoJS.SHA256(value).toString();
     return encrypted;
   }
 }
