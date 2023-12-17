329dbae4d41bbd36c7ca64376bc9e06adfa15717
nyrta1 <nurtai_2005@mail.ru>
2023-12-18 03:12:50 +0600

    refactor: [spring-boot] unused classes and methods have been removed.

--- src/main/java/com/example/youdown/converters/FileConverter.java
+++ src/main/java/com/example/youdown/converters/FileConverter.java
@@ -7,13 +7,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileConverter {
-    public static String convertToRoundedMB(Double value) {
-        if (value == null) {
-            return "N/A";
-        }
-        return String.format("%.2f MB", Math.round(value * 100.0) / 100.0);
-    }
-
     public static byte[] convertFileToBytes(File file) throws IOException {
         Path filePath = Paths.get(file.getAbsolutePath());
         return Files.readAllBytes(filePath);
